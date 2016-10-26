ea3802470ef8f4b442a12d29b7bd428c03403491
valery <valery.fremaux@gmail.com>
2016-10-26 23:55:38 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016040300;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015111100;        // Requires this Moodle version.
+$plugin->requires  = 2016051900;        // Requires this Moodle version.
 $plugin->component = 'auth_ext';         // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.0.0 Build 2016040300)';
+$plugin->release = '3.1.0 Build 2016040300)';
 $plugin->maturity = MATURITY_BETA;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.0.0000';
+$plugin->codeincrement = '3.1.0000';
