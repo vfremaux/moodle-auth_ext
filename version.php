77f35e7120ad992a80e276f4c960d8e5139b22d0
valery <valery.fremaux@gmail.com>
2016-10-26 23:55:12 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -20,17 +20,17 @@
  * @package     auth_ext
  * @category    auth
  * @author      Valery Fremaux <valery.fremaux@gmail.com>
- * @copyright   2011 Valery Fremaux (http://www.mylearningfactory.com)
+ * @copyright   2012 Valery Fremaux (http://www.mylearningfactory.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015100800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015050500;        // Requires this Moodle version.
-$plugin->component = 'auth_ext';        // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_RC;
-$plugin->release = '2.9.0 (Build 2015100800)';
+$plugin->version   = 2016040300;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2015111100;        // Requires this Moodle version.
+$plugin->component = 'auth_ext';         // Full name of the plugin (used for diagnostics).
+$plugin->release = '3.0.0 Build 2016040300)';
+$plugin->maturity = MATURITY_BETA;
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.9.0000';
+$plugin->codeincrement = '3.0.0000';
